8c2572fa818160bb7c0a041ae5c36b49254afadf
Bifabig <biftubig@gmail.com>
2023-05-31 18:29:58 +0300

    implement basic operation

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,58 @@
+import { useState } from 'react';
 import Display from './Display';
 import InputValues from './InputValues';
 
-const Calculator = () => (
-  <div className="container">
-    <Display />
-    <InputValues
-      values={[
-        'AC',
-        '+/-',
-        '%',
-        '÷',
-        '7',
-        '8',
-        '9',
-        'x',
-        '4',
-        '5',
-        '6',
-        '-',
-        '1',
-        '2',
-        '3',
-        '+',
-        '0',
-        '.',
-        '=',
-      ]}
-    />
-  </div>
-);
+const Calculator = () => {
+  const [currentValue, setCurrentValue] = useState('');
+  const [data, setData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  // if (result === null) {
+  //   setData(result);
+  // }
+
+  // if (currentValue === '') {
+  //   setData({ ...data, next: currentValue });
+  // }
+
+  // console.log(result);
+  console.log(data);
+
+  // console.log(currentValue);
+  return (
+    <div className="container">
+      <Display currentValue={currentValue} />
+      <InputValues
+        data={data}
+        setData={setData}
+        setCurrentValue={setCurrentValue}
+        values={[
+          'AC',
+          '+/-',
+          '%',
+          '÷',
+          '7',
+          '8',
+          '9',
+          'x',
+          '4',
+          '5',
+          '6',
+          '-',
+          '1',
+          '2',
+          '3',
+          '+',
+          '0',
+          '.',
+          '=',
+        ]}
+      />
+    </div>
+  );
+};
 
 export default Calculator;
--- src/components/InputValues.js
+++ src/components/InputValues.js
@@ -1,16 +1,49 @@
 import PropTypes from 'prop-types';
+import calculate from '../logic/calculate';
 
-const InputValues = ({ values }) => (
-  <div className="values">
-    {values.map((value) => (
-      <div key={value} className="value">
-        {value}
-      </div>
-    ))}
-  </div>
-);
+const InputValues = ({
+  data, setData, setCurrentValue, values,
+}) => {
+  const onClickHandler = (value) => {
+    const result = calculate(data, value);
+    console.log(result);
+
+    if (result.operation && !result.next) {
+      setData({ total: result.total, next: null, operation: result.operation });
+    } else if (result.operation && result.next) {
+      setData({
+        total: result.total,
+        next: result.next,
+        operation: result.operation,
+      });
+    } else {
+      setData({ ...data, next: result.next });
+    }
+    setCurrentValue(value);
+  };
+  return (
+    <div className="values">
+      {values.map((value) => (
+        <button
+          onClick={() => onClickHandler(value)}
+          type="button"
+          key={value}
+          className="value"
+        >
+          {value}
+        </button>
+      ))}
+    </div>
+  );
+};
 
 InputValues.defaultProps = { values: [] };
+InputValues.defaultProps = { data: {} };
+// InputValues.defaultProps = { currentValue: [] };
 InputValues.propTypes = { values: PropTypes.arrayOf(PropTypes.string) };
+InputValues.propTypes = { data: PropTypes.objectOf(PropTypes.string) };
+// InputValues.propTypes = { currentValue: PropTypes.arrayOf(PropTypes.string) };
+InputValues.propTypes = { setCurrentValue: PropTypes.func.isRequired };
+InputValues.propTypes = { setData: PropTypes.func.isRequired };
 
 export default InputValues;
